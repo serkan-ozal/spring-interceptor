41275b3cd4d71300ec64006dd8b6657356dfe21d
unknown <Asus@Asus-Bilgisayar.(none)>
2013-05-17 21:03:46 +0300

    Initial commit

---
+++ src/main/java/org/springframework/interceptor/service/InterceptorServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.interceptor.service;
+
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author SERKAN OZAL
+ *         
+ *         E-Mail: <a href="mailto:serkanozal86@hotmail.com">serkanozal86@hotmail.com</a>
+ *         GitHub: <a>https://github.com/serkan-ozal</a>
+ */
+@Service("interceptorService")
+@DependsOn({"springUtil"})
+public class InterceptorServiceImpl implements InterceptorService {
+
+}
